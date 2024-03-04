516929d4ae8e6f051bb5e23c46f69b6c3dfc1d70
Eli Cohavi <159360081+EliCohavi@users.noreply.github.com>
2024-03-04 13:42:53 -0700

    Add files via upload
    Added Audio on 'ENTER'
    Added HD40307g
    Fixed planet HUD position remapping

--- main.js
+++ main.js
@@ -5,12 +5,37 @@ import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 
+/*
+Music used
+Quantum Particles - by Jonny Easton
+Link: https://www.youtube.com/watch?v=w_ms_Xe0Jtk
+*/
+
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.z = -950;
+camera.position.z = 50;
 camera.lookAt(0, 0, -10000);
 scene.add(camera);
+
+//AudioListener
+const audioListener = new THREE.AudioListener();
+camera.add(audioListener);
+
+const ambient = new THREE.Audio(audioListener);
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load('Assets/Audio/Ambient.mp3', function (buffer) {
+    ambient.setBuffer(buffer);
+    ambient.setLoop(true);
+    ambient.setVolume(0.1);
+});
+
+let playAmbientBoolean = false;
+function playAmbient() {
+    ambient.play();
+    console.log("Ambient is playing");
+}
 
 
 //Game Controller Integration
@@ -54,6 +79,8 @@ function controllerInput() {
         downPressed = buttons[13].pressed;
         leftPressed = buttons[14].pressed;
         rightPressed = buttons[15].pressed;
+
+
 
         const stickDeadZone = 0.2;
         leftRightValue = gamepad.axes[2];
@@ -104,19 +131,15 @@ for (let i = 0; i < 10000; i++) {
 
     stars[i].position.x = THREE.MathUtils.randFloatSpread(-20, 20);
     stars[i].position.y = THREE.MathUtils.randFloatSpread(-20, 20);
-    stars[i].position.z = THREE.MathUtils.randFloat(100, -1000);
+    stars[i].position.z = THREE.MathUtils.randFloat(100, -1500);
     //console.log(stars[i].position.z); -- DEBUG
 
     scene.add(stars[i]);
 }
 
 
-//const controls = new OrbitControls(camera, renderer.domElement);
-//controls.enableDamping = true;
-//controls.autoRotate = true;
-
 // movement - please calibrate these values
-var ySpeed = 0.2; //Every 1.0 move is 1000 years
+var zSpeed = 0.15;
 
 
 //Text Attributes
@@ -163,34 +186,9 @@ const font = loader.load(
 );
 
 //creates global version of model
-var Kepler186f;
 var movementYearMultiplier = 200.0;
-var k186ZPosition = -(985492.0 / ((1 / ySpeed) * movementYearMultiplier));
-
-console.log(k186ZPosition);
-//load model
-new GLTFLoader()
-    .load('Assets/Planets/kepler-186f/kepler_186f-v1.glb',
-        function (kepler186) {
-            kepler186.scene.scale.set(1, 1, 1);
-            Kepler186f = kepler186.scene.getObjectByName('Scene');
-            kepler186.scene.position.set(-1.5, 0, k186ZPosition);
-            kepler186.scene.rotation.y = Math.PI / 2;
-
-            const model = kepler186.scene.children[0];
-            model.traverse(child => {
-                if (child.isMesh) {
-                    child.castShadow = true;
-                    child.receiveShadow = true;
-                }
-            })
-
-            //scene.add(kepler186.scene);
-            //How to catch other meshes of model
-
-        }
-    );
-;
+var k186ZPosition = -(985492.0 / 1000);
+var HD40307ZPosition = -(71354.0 / 1000);
 
 
 //Lights
@@ -202,6 +200,15 @@ K186light.shadow.camera.near = 1;
 K186light.shadow.camera.far = 10000;
 K186light.shadow.mapSize.set(1024, 1024);
 scene.add(K186light);
+
+const HD40307Light = new THREE.PointLight(0xffffff, 3, 10, 0);
+HD40307Light.position.set(3, 0, HD40307ZPosition + 4.5);
+HD40307Light.castShadow = true;
+HD40307Light.receiveShadow = true;
+HD40307Light.shadow.camera.near = 1;
+HD40307Light.shadow.camera.far = 10000;
+HD40307Light.shadow.mapSize.set(1024, 1024);
+scene.add(HD40307Light);
 
 const light2 = new THREE.AmbientLight(0xffffff, 0.003);
 scene.add(light2);
@@ -223,11 +230,16 @@ let yearsGeo;
 let years;
 let K186textGeo;
 let K186text;
+let HUDHD40307Geo;
+let HD40307textGeo;
+let HD40307text;
+
 
 
 //add text HUDs
 HUDYearsGeo = new THREE.Group();
 HUDK186Geo = new THREE.Group();
+HUDHD40307Geo = new THREE.Group();
 
 //All Text Generations
 function textGeneration(font) {
@@ -291,21 +303,61 @@ secrets and ponder the vastness of the cosmos.`, {
     K186text.position.y = -0.6;
     K186text.position.z = -1;
 
-    //Add Box to hud
-    // HUDK186Geo.add(k186box);
 
     //Add to hud
     HUDK186Geo.add(K186text);
 
 
+    //HD 40307g
+    HD40307textGeo = new TextGeometry(
+        `Discovered by Mikko Tuomi on October 28, 2012, HD 40307g
+orbits within the HD 40307 system in the Pictor constellation,
+situated 42 light-years away from Earth. With a mass
+approximately 7.09 times that of Earth and a radius around
+2.39 times larger, HD 40307g emerges as a distant enigma,
+beckoning exploration and inquiry. Postulated to be a
+Neptune-like ice giant, HD 40307g likely possesses a surface
+gravity estimated to be around 4.5 times greater than Earth's. 
+While little is known about its terrain and temperature, its
+distant allure captivates the imagination. Despite the journey's
+daunting duration—71,354 years at 395,000 mph—HD 40307g
+stands as a testament to the boundless wonders awaiting
+discovery in the vastness of the cosmos.`, {
+
+        font: font,
+
+        size: size / 7,
+        height: height / 8,
+        curveSegments: curveSegments,
+
+        bevelThickness: bevelThickness / 8,
+        bevelSize: bevelSize / 8,
+        //bevelEnabled: bevelEnabled / 8,
+
+    }).center();
+
+    //Text Object Creation
+    HD40307text = new THREE.Mesh(HD40307textGeo, materials);
+    HD40307text.position.x = -0.88;
+    HD40307text.position.y = -0.6;
+    HD40307text.position.z = -1;
+
+
+    //Add to hud
+    HUDHD40307Geo.add(HD40307text);
+
+
+
 }
 
 HUDYearsGeo.position.set(1.1, 0.65, 0);
-HUDK186Geo.position.set(1.5, 0.65, 0);
 
 camera.add(HUDYearsGeo);
+camera.add(HUDK186Geo);
+camera.add(HUDHD40307Geo);
 
 //HUD Boxes
+//K186
 const k186BoxGeo = new THREE.BoxGeometry(1.85, .8, 0.01);
 const k186BoxMat = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.4 });
 const k186box = new THREE.Mesh(k186BoxGeo, k186BoxMat);
@@ -313,8 +365,21 @@ k186box.position.x = -0.775;
 k186box.position.y = -0.6;
 k186box.position.z = -1;
 HUDK186Geo.add(k186box);
-
 HUDK186Geo.position.x = 3.5;
+HUDK186Geo.position.y = 0.5;
+
+//HD40307
+const HD40307BoxGeo = new THREE.BoxGeometry(1.85, .9, 0.01);
+const HD40307BoxMat = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.4 });
+const HD40307Box = new THREE.Mesh(HD40307BoxGeo, HD40307BoxMat);
+HD40307Box.position.x = -0.775;
+HD40307Box.position.y = -0.6;
+HD40307Box.position.z = -1;
+HUDHD40307Geo.add(HD40307Box);
+
+HUDHD40307Geo.position.x = 3.5;
+HUDHD40307Geo.position.y = 0.5;
+
 
 function updateYears() {
 
@@ -333,13 +398,14 @@ function remap(value, istart, istop, ostart, ostop) {
     return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
 }
 
-HUDK186Geo.position.x, 
+
+
 
 var rotationSpeed = 0.01;
 //For Controller
 function moveCamera() {
     if (upPressed) { //between -1 and 0
-        camera.position.z += (0.5 * upDownValue * ySpeed);
+        camera.position.z += (0.5 * upDownValue * zSpeed);
         if (yearsTraveled < 0) {
             yearsTraveled = 0;
         }
@@ -347,12 +413,11 @@ function moveCamera() {
             yearsTraveled += Math.abs((0.5 * upDownValue * movementYearMultiplier));
             HUDtext = "Years Traveled: ", Math.round(yearsTraveled);
         }
-
-        console.log(HUDtext);
         console.log(camera.position.z);
+
     }
     if (downPressed) { //between 0 and 1
-        camera.position.z += (0.5 * upDownValue * ySpeed);
+        camera.position.z += (0.5 * upDownValue * zSpeed);
         if (yearsTraveled < 0) {
             yearsTraveled = 0;
         }
@@ -360,15 +425,17 @@ function moveCamera() {
             yearsTraveled -= Math.abs((0.5 * upDownValue * movementYearMultiplier));
             HUDtext = "Years Traveled: ", Math.round(yearsTraveled);
         }
-
-        console.log(HUDtext);
         console.log(camera.position.z);
+        //console.log(HUDtext);
+
     }
     if (leftPressed) {
         K186f.rotation.y -= Math.abs((leftRightValue * rotationSpeed));
+        HD40307g.rotation.y -= Math.abs((leftRightValue * rotationSpeed));
     }
     if (rightPressed) {
         K186f.rotation.y += (leftRightValue * rotationSpeed);
+        HD40307g.rotation.y += (leftRightValue * rotationSpeed);
     }
     /*  These are all using the axes value paired with the rotationSpeed as a multiplier to create variable speeds based off input.
         Absolute value is added so direction can reverse (Rules of Math for adding and subtracting positive and negative integers).*/
@@ -378,29 +445,22 @@ function moveCamera() {
         textGeneration(font);
     }
 
-    //Kepler-186f HUD
-    if (Math.abs(camera.position.z) > Math.abs(k186ZPosition) - 4 && Math.abs(camera.position.z) < Math.abs(k186ZPosition) + 2) {
-        if (camera.getObjectByName(HUDK186Geo) == null) {
-            camera.add(HUDK186Geo);
-            //console.log('K186f text has been added');
-
-        }
+    //KEPLER186 HUD
+    if (Math.abs(camera.position.z) > Math.abs(k186ZPosition) - 9 && Math.abs(camera.position.z) < Math.abs(k186ZPosition) - 4) {
+        HUDK186Geo.position.x = remap(Math.abs(camera.position.z), Math.abs(k186ZPosition) - 9, Math.abs(k186ZPosition) - 4, 3.5, 1.4);
     }
-    if (Math.abs(camera.position.z) < Math.abs(k186ZPosition) - 4 || Math.abs(camera.position.z) > Math.abs(k186ZPosition) + 2) {
-        camera.remove(HUDK186Geo);
-        //console.log('K186f text has been removed');
+    if (Math.abs(camera.position.z) > Math.abs(k186ZPosition) && Math.abs(camera.position.z) < Math.abs(k186ZPosition) + 2) {
+        HUDK186Geo.position.x = remap(Math.abs(camera.position.z), Math.abs(k186ZPosition), Math.abs(k186ZPosition) + 2, 1.4, 3.5);
     }
 
-    if (Math.abs(camera.position.z) > Math.abs(k186ZPosition) - 4 && Math.abs(camera.position.z) < Math.abs(k186ZPosition) - 0.5) {
-        HUDK186Geo.position.x -= Math.abs(upDownValue) * 0.02;
-        console.log('Geo decreases');
+    //HD40207 HUD
+    if (Math.abs(camera.position.z) > Math.abs(HD40307ZPosition) - 11 && Math.abs(camera.position.z) < Math.abs(HD40307ZPosition) - 6) {
+        HUDHD40307Geo.position.x = remap(Math.abs(camera.position.z), Math.abs(HD40307ZPosition) - 11, Math.abs(HD40307ZPosition) - 6, 3.5, 1.4);
     }
-    if (Math.abs(camera.position.z) < Math.abs(k186ZPosition) + 2 && Math.abs(camera.position.z) > Math.abs(k186ZPosition) + 0.5) {
-        HUDK186Geo.position.x += Math.abs(upDownValue) * 0.02;
-        console.log('Geo increases');
+    if (Math.abs(camera.position.z) > Math.abs(HD40307ZPosition) && Math.abs(camera.position.z) < Math.abs(HD40307ZPosition) + 2) {
+        HUDHD40307Geo.position.x = remap(Math.abs(camera.position.z), Math.abs(HD40307ZPosition), Math.abs(HD40307ZPosition) + 2, 1.4, 3.5);
     }
 
-    HUDK186Geo.position.x = map(Math.abs(camera.position.z),)
 
 }
 
@@ -416,27 +476,35 @@ function onDocumentKeyDown(event) {
         textGeneration(font);
     }
     if (keyCode == 87) { //UP KEY
-        camera.position.z -= ySpeed;
-        //console.log(camera.position.z); DEBUG
+        camera.position.z -= zSpeed;
+        console.log(camera.position.z);
         yearsTraveled += movementYearMultiplier;
     } else if (keyCode == 83) { //DOWN KEY
-        camera.position.z += ySpeed;
-        //console.log(camera.position.z); DEBUG
+        camera.position.z += zSpeed;
+        console.log(camera.position.z);
         yearsTraveled -= movementYearMultiplier;
     }
 
-    //Kepler-186f HUD
-    if (camera.position.z > Math.abs(k186ZPosition) - 4 && camera.position.z < Math.abs(k186ZPosition) + 2) {
-        if (camera.getObjectByName(HUDK186Geo) == null) {
-            camera.add(HUDK186Geo);
-            console.log('K186f text has been added');
-        }
+    if (keyCode == 13) {
+        playAmbient();
     }
-    if (camera.position.z < Math.abs(k186ZPosition) - 4 || camera.position.z > Math.abs(k186ZPosition) + 2) {
-        //if (camera.getObjectByName(HUDK186Geo) != null) {
-        camera.remove(HUDK186Geo);
-        console.log('K186f text has been removed');
+
+    //K186 HUD
+    if (Math.abs(camera.position.z) > Math.abs(k186ZPosition) - 9 && Math.abs(camera.position.z) < Math.abs(k186ZPosition) - 4) {
+        HUDK186Geo.position.x = remap(Math.abs(camera.position.z), Math.abs(k186ZPosition) - 9, Math.abs(k186ZPosition) - 4, 3.5, 1.4);
     }
+    if (Math.abs(camera.position.z) > Math.abs(k186ZPosition) && Math.abs(camera.position.z) < Math.abs(k186ZPosition) + 2) {
+        HUDK186Geo.position.x = remap(Math.abs(camera.position.z), Math.abs(k186ZPosition), Math.abs(k186ZPosition) + 2, 1.4, 3.5);
+    }
+
+    //HD40207 HUD
+    if (Math.abs(camera.position.z) > Math.abs(HD40307ZPosition) - 11 && Math.abs(camera.position.z) < Math.abs(HD40307ZPosition) - 6) {
+        HUDHD40307Geo.position.x = remap(Math.abs(camera.position.z), Math.abs(HD40307ZPosition) - 11, Math.abs(HD40307ZPosition) - 6, 3.5, 1.4);
+    }
+    if (Math.abs(camera.position.z) > Math.abs(HD40307ZPosition) && Math.abs(camera.position.z) < Math.abs(HD40307ZPosition) + 2) {
+        HUDHD40307Geo.position.x = remap(Math.abs(camera.position.z), Math.abs(HD40307ZPosition), Math.abs(HD40307ZPosition) + 2, 1.4, 3.5);
+    }
+
 };
 
 //K186f Texture
@@ -455,10 +523,32 @@ K186f.receiveShadow = true;
 K186f.castShadow = true;
 scene.add(K186f);
 
+//HD 40307g Texture
+const HD40307Texture = new THREE.TextureLoader().load(
+    "Assets/Planets/hd-40307g/textures/Material_baseColor.png");
+HD40307Texture.wrapS = THREE.RepeatWrapping;
+HD40307Texture.wrapT = THREE.RepeatWrapping;
+HD40307Texture.repeat.set(1, 1);
+
+const HD40307Geo = new THREE.SphereGeometry(4, 64, 64);
+const HD40307Mat = new THREE.MeshPhysicalMaterial({ map: HD40307Texture, color: 0xffffff });
+const HD40307g = new THREE.Mesh(HD40307Geo, HD40307Mat);
+HD40307g.position.set(-5, 0, HD40307ZPosition);
+HD40307g.rotation.z = Math.PI / 6;
+HD40307g.receiveShadow = true;
+HD40307g.castShadow = true;
+scene.add(HD40307g);
+
+
+
+
 function rotatePlanets() {
     K186f.rotation.y += 0.0005;
+    HD40307g.rotation.y += 0.0003;
+
 
 }
+
 
 function animate() {
     requestAnimationFrame(animate);
@@ -475,9 +565,6 @@ function animate() {
     }
     //controls.update();
     renderer.render(scene, camera);
-    if (Kepler186f) {
-        rotatePlanets();
-    }
 
     updateCamera();
 }
